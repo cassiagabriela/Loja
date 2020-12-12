c9a27fca91330939e9540b90b13a455491e2b774
cassiagabriela <cassiagabrielasp@gmail.com>
2020-12-12 16:41:46 -0300

    model do produto criado

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    referencia = models.CharField(max_length=5)
+    preco = models.CharField(max_length=8)
+    imagem = models.ImageField(null=True)
+
+    def _str_ (self):
+        return self.titulo
+
+    class Meta:
+        db_table = 'Produto'
+
