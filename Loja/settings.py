aa1dbb79f0eb5308d1cb1eb9e511dcaed5ed22b2
cassiagabriela <cassiagabrielasp@gmail.com>
2020-12-12 15:24:05 -0300

    login

--- Loja/settings.py
+++ Loja/settings.py
@@ -118,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- Loja/urls.py
+++ Loja/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from contas import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -12,3 +14,5 @@ urlpatterns = [
     path('', views.index)
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
